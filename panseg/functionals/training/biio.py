55f23c7e0ce164e759d65738102941ff680e7617
Kai Riedmiller <kai.riedmiller@iwr.uni-heidelberg.de>
2026-05-27 16:41:09 +0200

    fix: wip

--- panseg/functionals/training/biio.py
+++ panseg/functionals/training/biio.py
@@ -10,12 +10,37 @@ from bioimageio.spec.model.v0_5 import (
     TensorId,
 )
 
-dim = "3d"
-if dim == "3d":
-    axes = [SpaceInputAxis(id=AxisId("z"), size=ParameterizedSize)]
 
-input_desc = InputTensorDescr(
-    description="model input",
-    axes=axes,
-    id=TensorId(),
-)
+def make_model_description(
+    dim="3d",
+    scale=(1.0, 1.0, 1.0),
+):
+    if dim == "3d":
+        axes = [
+            SpaceInputAxis(
+                id=AxisId("z"), size=ParameterizedSize(min=32, step=1), scale=scale[0]
+            ),
+            SpaceInputAxis(
+                id=AxisId("y"), size=ParameterizedSize(min=32, step=1), scale=scale[1]
+            ),
+            SpaceInputAxis(
+                id=AxisId("x"), size=ParameterizedSize(min=32, step=1), scale=scale[2]
+            ),
+        ]
+    elif dim == "2d":
+        axes = [
+            SpaceInputAxis(
+                id=AxisId("y"), size=ParameterizedSize(min=32, step=1), scale=scale[1]
+            ),
+            SpaceInputAxis(
+                id=AxisId("x"), size=ParameterizedSize(min=32, step=1), scale=scale[2]
+            ),
+        ]
+    else:
+        raise ValueError("Unknown dimension")
+
+    input_desc = InputTensorDescr(
+        description="model input",
+        axes=axes,
+        id=TensorId(),
+    )
